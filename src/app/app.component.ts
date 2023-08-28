097fe356da4e0abc17bf1816f572ab33fd45091c
Gabriel Lovric <gabriel.lovric@solyze.com>
2023-08-28 14:37:16 +0200

    add ag grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'trading-grid';
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    { field: 'company', headerName: 'Company' },
+    { field: 'description', headerName: 'Description' },
+    { field: 'initial_price', headerName: 'Initial Price' },
+    { field: 'price_2002', headerName: 'Price 2002' },
+    { field: 'price_2007', headerName: 'Price 2007' },
+    { field: 'symbol', headerName: 'Symbol' },
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+
+  // Data that gets displayed in the grid
+  public rowData$!: Observable<any[]>;
+
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+  constructor(private http: HttpClient) {}
+
+  // Example load data from server
+  onGridReady(params: GridReadyEvent) {
+    this.rowData$ = this.http.get<any[]>(
+      'https://gist.githubusercontent.com/tanveery/4ac939d2ad27954da4c8db13e10ef7bd/raw/0f3a58c5735553515aa7c826cb37f58da387be2c/sample-stocks-data.json'
+    );
+  }
+
+  // Example of consuming Grid Event
+  onCellClicked(e: CellClickedEvent): void {
+    console.log('cellClicked', e);
+  }
+
+  // Example using Grid's API
+  clearSelection(): void {
+    this.agGrid.api.deselectAll();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, AgGridModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
